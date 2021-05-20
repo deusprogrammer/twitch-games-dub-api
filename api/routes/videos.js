3bd36b78cbf7a5b584280f00bfe401f4c537041b
Michael C. Main <deusprogrammer@gmail.com>
2021-05-20 15:45:20 -0500

    Added get single and get mp4

--- api/routes/videos.js
+++ api/routes/videos.js
@@ -49,10 +49,36 @@ router.route("/")
         }
     });
 
-router.route("/:id/zip")
+router.route("/:id")
+    .get(async (request, response) => {
+        try {
+        let video = await Videos.findOne({_id: request.params.id});
+        return response.json(video);
+        } catch (error) {
+            console.error(error);
+            response.status(500);
+            return response.send(error);
+        }
+    });
+
+router.route("/:id/mp4")
     .get(async (request, response) => {
         try {
         let video = await Videos.findOne({_id: request.params.id})
+        let byteStream = await readFile(video.videoPath);
+        response.header("content-type", "video/mp4");
+        return response.sendFile(byteStream);
+        } catch (error) {
+            console.error(error);
+            response.status(500);
+            return response.send(error);
+        }
+    });
+
+router.route("/:id/zip")
+    .get(async (request, response) => {
+        try {
+        let video = await Videos.findOne({_id: request.params.id});
         let byteStream = await readFile(video.videoPath);
         let dataUri = await createPayloadZip(byteStream, video.subtitles);
         return response.json({
